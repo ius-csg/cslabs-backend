6068980f8fcbee485fb6aed230687d8f60cc7d03
Jason Gallavin <jasongallavin@gmail.com>
2019-09-25 21:34:54 -0400

    Add migrations for new models

--- cslabs-backend/Migrations/20190926012932_AddUserAndLabModels.Designer.cs
+++ cslabs-backend/Migrations/20190926012932_AddUserAndLabModels.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CSLabsBackend.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CSLabsBackend.Migrations
 {
     [DbContext(typeof(DefaultContext))]
-    partial class DefaultContextModelSnapshot : ModelSnapshot
+    [Migration("20190926012932_AddUserAndLabModels")]
+    partial class AddUserAndLabModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
