6211397bc894ef80a94a21899035c0d45774b094
Jason Gallavin <jasongallavin@gmail.com>
2019-11-04 21:12:21 -0500

    Add remaining proxmox functions

--- cslabs-backend/Proxmox/ProxmoxApi.cs
+++ cslabs-backend/Proxmox/ProxmoxApi.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Newtonsoft.Json;
 using Rundeck;
 using  RundeckClient = Rundeck.Client;
@@ -14,17 +15,41 @@ namespace CSLabsBackend.Proxmox
             this.client = new RundeckClient(scheme, host, authToken);
         }
 
+
+        public ExecutionParams getVmIdParams(int vmId)
+        {
+            return new ExecutionParams() {options = new Dictionary<string, string>() {
+                ["VmId"] = vmId.ToString()
+            }};
+        }
+
         public async Task<TicketResponse> GetTicket(int vmId)
         {
-            var output = await this.client.RunJobAndGetOutput("***REMOVED***",
-                new ExecutionParams()
-                {
-                    options = new Dictionary<string, string>()
-                    {
-                        ["VmId"] = vmId.ToString()
-                    }
-                });
+            var output = await this.client.RunJobAndGetOutput("***REMOVED***", getVmIdParams(vmId));
             return JsonConvert.DeserializeObject<TicketResponse>(output);
+        }
+        
+        public async Task StartVM(int vmId)
+        {
+            await this.client.RunJobAndGetOutput("***REMOVED***", getVmIdParams(vmId));
+        }
+        
+        
+        public async Task StopVM(int vmId)
+        {
+            await this.client.RunJobAndGetOutput("***REMOVED***", getVmIdParams(vmId));
+        }
+        
+        
+        public async Task ShutdownVm(int vmId)
+        {
+            await this.client.RunJobAndGetOutput("***REMOVED***", getVmIdParams(vmId));
+        }
+        
+        public async Task<int> CloneTemplate(int vmId)
+        {
+            var output = await this.client.RunJobAndGetOutput("***REMOVED***", getVmIdParams(vmId));
+            return int.Parse(output);
         }
     }
 }
