19ccfb9cad6bb4a7ea0a60cee32291ff5473faa9
Jason Gallavin <jasongallavin@gmail.com>
2019-11-08 22:58:21 -0500

    Add Console Logging

--- cslabs-backend/Program.cs
+++ cslabs-backend/Program.cs
@@ -19,6 +19,11 @@ namespace CSLabsBackend
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+//                .ConfigureLogging(logging =>
+//                {
+//                    logging.ClearProviders();
+//                    logging.AddConsole();
+//                })
                 .UseStartup<Startup>();
     }
 }
