f45aa91e8f4c575c802c0843e14134e6161f9d28
Jason Gallavin <jasongallavin@gmail.com>
2019-12-01 19:22:59 -0500

    Remove Unused project files and fix project references.

--- Proxmox/ExecutionParams.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Rundeck
-{
-    public class ExecutionParams
-    {
-        public Dictionary<string, string> options { get; set; } = new Dictionary<string, string>();
-    }
-}
--- Proxmox/Project.cs
+++
@@ -1,9 +0,0 @@
-namespace Rundeck
-{
-    public class Project
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Url { get; set; }
-    }
-}
