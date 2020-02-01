b37fdc2d4550e7ed488e21cb72d96a05827d51fd
Barel Musenga <barel.musenga87@gmail.com>
2020-02-01 16:17:01 -0500

    Add migration to add the EndDateTime property

---
+++ CSLabs.Api/Migrations/20200201211359_AddEndDateTimeToUserLab.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CSLabs.Api.Migrations
+{
+    public partial class AddEndDateTimeToUserLab : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "end_date_time",
+                table: "user_labs",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_labs_end_date_time",
+                table: "user_labs",
+                column: "end_date_time");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_user_labs_end_date_time",
+                table: "user_labs");
+
+            migrationBuilder.DropColumn(
+                name: "end_date_time",
+                table: "user_labs");
+        }
+    }
+}
