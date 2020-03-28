5fb9c51604e3df44a7c177eff12501d74ed6c1af
Jason Gallavin <jasongallavin@gmail.com>
2020-03-28 14:17:42 -0400

    Update user types

--- CSLabs.Api/Models/Enums/UserType.cs
+++ CSLabs.Api/Models/Enums/UserType.cs
@@ -1,11 +1,10 @@
-﻿using System;
+﻿
 namespace CSLabs.Api.Models.Enums
 {
     public static class UserType
     {
         public const string Guest = "guest";
-        public const string Member = "member";
-        public const string Officer = "officer";
-        public const string Staff = "Staff";
+        public const string Admin = "admin";
+        public const string Creator = "creator";
     }
 }
