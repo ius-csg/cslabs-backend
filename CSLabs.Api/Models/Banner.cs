81043cc176fb9f7080c4819f3985b2d1523992a3
astin <astiner21@gmail.com>
2021-08-30 21:27:01 -0400

    Removed methods not needed and fixed issue with datatime

--- CSLabs.Api/Models/Banner.cs
+++ CSLabs.Api/Models/Banner.cs
@@ -10,22 +10,22 @@ namespace CSLabs.Api.Models
     {
 
         public int Id { get; set; }
-        [Required]
-        
-        public string type { get; set; }
-        [Column(TypeName = "TEXT")]
-        [Required]
-        
-        public string description { get; set; }
+        [Column(TypeName = "TEXT")]//need to decide on a enum
+        [Required] 
+        public string Type { get; set; }
+       
         [Column(TypeName = "VARCHAR(255)")]
         [Required]
+        public string Description { get; set; }
         
-       public int RequiredNumOfModules { get; set; }
-
+        [Column(TypeName = "datetime")]
+        [Required]
+        public string timestamp { get; set; }
+       
         public static void OnAlertCreation(ModelBuilder builder)
         {
             builder.TimeStamps<Banner>();
-            builder.Unique<Banner>(u => u.type);
+            builder.Unique<Banner>(u => u.Type);
         }
        
     }
