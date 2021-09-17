89fa49621d1dc945069d52daf48d2b783173b974
Zion Emond <zionem7@gmail.com>
2021-09-17 14:34:32 -0400

    started adding Db model for SystemStatus

--- CSLabs.Api/Models/DefaultContext.cs
+++ CSLabs.Api/Models/DefaultContext.cs
@@ -43,6 +43,8 @@ namespace CSLabs.Api.Models
         public DbSet<HypervisorVmTemplate> HypervisorVmTemplates { get; set; }
         
         public DbSet<VmTemplate> VmTemplates { get; set; }
+        
+        public DbSet<SystemStatus> SystemStatus { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
        {
---
+++ CSLabs.Api/Models/SystemStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace CSLabs.Api.Models
+{
+    public class SystemStatus
+    {
+        public int HypervisorId { get; set; }
+        public bool Quorum { get; set; }
+        public int HypervisorNodesUp { get; set; }
+        public int TotalNodes { get; set; }
+    }
+}
