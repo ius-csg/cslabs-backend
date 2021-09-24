0c37e56a57ba996b8e6fe630519c5d86ed40ae53
Zion Emond <zionem7@gmail.com>
2021-09-24 09:23:15 -0400

    replaced RestorationTime with two DateTime type variables

--- CSLabs.Api/Models/Maintenance.cs
+++ CSLabs.Api/Models/Maintenance.cs
@@ -1,10 +1,12 @@
-﻿namespace CSLabs.Api.Models
+﻿using System;
+
+namespace CSLabs.Api.Models
 {
     public class Maintenance
     {
         public int Id { get; set; }
         public bool IsMaintenanceMode { get; set; }
-        public bool IsRestorationTimeKnown { get; set; }
-        public int RestorationTime { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
     }
 }
