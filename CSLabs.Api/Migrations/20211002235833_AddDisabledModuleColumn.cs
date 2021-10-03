40fcb577eac318b03b9ad631bfb83ede52e0db0c
Justin Hurst <hurstjustin502@gmail.com>
2021-10-02 20:01:03 -0400

    Added migrations for disabled module column

---
+++ CSLabs.Api/Migrations/20211002235833_AddDisabledModuleColumn.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CSLabs.Api.Migrations
+{
+    public partial class AddDisabledModuleColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "disabled",
+                table: "modules",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "disabled",
+                table: "modules");
+        }
+    }
+}
