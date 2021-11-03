7853d71d8da6585c5707d17aa1260cb89b2ecd0d
Jason Gallavin <jasongallavin@gmail.com>
2021-11-02 23:29:07 -0400

    Remove unused code to meet code smell, Fix Migration issue with Raw SQL code

--- CSLabs.Api/Controllers/SystemMessageController.cs
+++ CSLabs.Api/Controllers/SystemMessageController.cs
@@ -1,13 +1,8 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using CSLabs.Api.Models;
 using CSLabs.Api.Services;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Server.IISIntegration;
 
 namespace CSLabs.Api.Controllers
 {
@@ -20,104 +15,11 @@ namespace CSLabs.Api.Controllers
         {
             Service = new SystemMessageService(deps.DatabaseContext, new TimeService());
         }
-
-        // GET: api/SystemMessage
+        
         [HttpGet]
         public async Task<ActionResult<IEnumerable<SystemMessage>>> GetSystemMessages()
         {
             return await Service.GetActiveMessages();
         }
-        
-        /*// GET: api/SystemMessage/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<SystemMessage>> GetSystemMessage(int id)
-        {
-            var systemMessage = await this.DatabaseContext.SystemMessages.FindAsync(id);
-
-            if (systemMessage == null)
-            {
-                return NotFound();
-            }
-
-            return systemMessage;
-            
-        }
-
-        // PUT: api/SystemMessage/5
-        [HttpPut("{id}")]
-        [Authorize] 
-         public async Task<IActionResult> PutSystemMessage(int id, SystemMessage systemMessage)
-        {
-            if (!GetUser().IsAdmin())
-            {
-                return Forbid("Access denied");
-            }
-
-            if (id != systemMessage.Id)
-            {
-                return BadRequest();
-            }
-
-            this.DatabaseContext.Entry(systemMessage).State = EntityState.Modified;
-
-            try
-            {
-                await this.DatabaseContext.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!SystemMessageExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/SystemMessage
-        [HttpPost]
-        public async Task<ActionResult<SystemMessage>> PostSystemMessage(SystemMessage systemMessage)
-        {
-            if (!GetUser().IsAdmin())
-            {
-                return Forbid("Access denied");
-            }
-
-            this.DatabaseContext.SystemMessages.Add(systemMessage);
-            await this.DatabaseContext.SaveChangesAsync();
-
-            return CreatedAtAction("GetSystemMessage", new { id = systemMessage.Id }, systemMessage);
-        }
-
-        // DELETE: api/SystemMessage/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<SystemMessage>> DeleteSystemMessage(int id)
-        {
-            if (!GetUser().IsAdmin())
-            {
-                return Forbid("Access denied");
-            }
-
-            var systemMessage = await this.DatabaseContext.SystemMessages.FindAsync(id);
-            if (systemMessage == null)
-            {
-                return NotFound();
-            }
-
-            this.DatabaseContext.SystemMessages.Remove(systemMessage);
-            await this.DatabaseContext.SaveChangesAsync();
-
-            return systemMessage;
-        }
-
-        private bool SystemMessageExists(int id)
-        {
-            return this.DatabaseContext.SystemMessages.Any(e => e.Id == id);
-        }*/
     }
 }
