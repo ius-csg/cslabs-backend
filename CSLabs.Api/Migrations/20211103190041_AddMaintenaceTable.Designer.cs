abbca02ba494565fe7c487e860e846f485242be9
Zion Emond <zionem7@gmail.com>
2021-11-03 15:17:54 -0400

    add migration

--- CSLabs.Api/Migrations/20211103190041_AddMaintenaceTable.Designer.cs
+++ CSLabs.Api/Migrations/20211103190041_AddMaintenaceTable.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CSLabs.Api.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CSLabs.Api.Migrations
 {
     [DbContext(typeof(DefaultContext))]
-    partial class DefaultContextModelSnapshot : ModelSnapshot
+    [Migration("20211103190041_AddMaintenaceTable")]
+    partial class AddMaintenaceTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
