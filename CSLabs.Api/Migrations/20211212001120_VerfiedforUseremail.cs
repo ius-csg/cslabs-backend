b7b8da29ddd05372db4a56555457680910994a95
astin <astiner21@gmail.com>
2021-12-11 19:31:36 -0500

    Created User email verification migration

---
+++ CSLabs.Api/Migrations/20211212001120_VerfiedforUseremail.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CSLabs.Api.Migrations
+{
+    public partial class VerfiedforUseremail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "verified",
+                table: "users",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "verified",
+                table: "users");
+        }
+    }
+}
