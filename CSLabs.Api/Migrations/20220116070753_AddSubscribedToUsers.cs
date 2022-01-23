b0ddddb9267e95b6c3a8a099c30b32abe1ea1e62
Zaid Hussain <kzhzaid02@gmail.com>
2022-01-23 11:16:02 -0500

    Update db to add subscribe column

---
+++ CSLabs.Api/Migrations/20220116070753_AddSubscribedToUsers.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSLabs.Api.Migrations
+{
+    public partial class AddSubscribedToUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "subscribed",
+                table: "users",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "subscribed",
+                table: "users");
+        }
+    }
+}
